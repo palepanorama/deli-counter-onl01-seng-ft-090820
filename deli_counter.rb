78b0e07f7a7cb6ec0a927693ef01a4849509536c
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 18:01:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 katz_deli = []
 
 
-def line(katz_deli)
+def line(a)
   line_array = []
-  if katz_deli.length == 0  
+  if a.length == 0  
     puts "The line is currently empty"
   else 
-    katz_deli.each_with_index(1) do |name, index|
+    a.each_with_index(1) do |name, index|
     line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join("")}"
