48cd80a9efbd09a9f95159d4796e16b414a7cc0e
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 17:55:01 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,25 @@
+katz_deli = []
+
+
+def line(katz_deli)
+  if katz_deli.length == 0  
+    "The line is currently empty"
+  elsif katz_deli.length >= 1
+    return "The line is currently: "
+    katz_deli.each do |name, index|
+      return "#{index + 1}. #{name}."
+    end
+  end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+def now_serving
+  puts "Now serving #{katz_deli[0]}!"
+  katz_deli.shift
+end
+end
