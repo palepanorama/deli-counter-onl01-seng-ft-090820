5cc88a609f5573bbb462569047c3d9cc670cdd89
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 18:06:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(x)
   if x.length == 0
     puts "The line is currently empty."
   else
-    x.each.with_index(1) do |name, index|
+    x.each.with_index do |name, index|
       line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
